178fff86ebd103544c176a048d811a2bd147f118
Mora <mlabisi98@gmail.com>
2022-10-31 13:55:11 -0700

    connecting mora's iphone

--- lib/main.dart
+++ lib/main.dart
@@ -81,10 +81,7 @@ class _RootState extends State<Root> {
           color: Colors.white,
           child: SizedBox(
             height: 56,
-            width: MediaQuery
-                .of(context)
-                .size
-                .width,
+            width: MediaQuery.of(context).size.width,
             child: Padding(
               padding: const EdgeInsets.only(left: 25.0, right: 25.0),
               child: Row(
@@ -119,17 +116,17 @@ class _RootState extends State<Root> {
               ),
             ),
           ),
-        )
-    );
+        ));
   }
 }
 
 class IconBottomBar extends StatelessWidget {
-  const IconBottomBar({Key? key,
-    required this.activeIcon,
-    required this.icon,
-    required this.selected,
-    required this.onPressed})
+  const IconBottomBar(
+      {Key? key,
+      required this.activeIcon,
+      required this.icon,
+      required this.selected,
+      required this.onPressed})
       : super(key: key);
   final IconData activeIcon;
   final IconData icon;
@@ -179,12 +176,11 @@ class SuggestionsAppBar extends StatelessWidget with PreferredSizeWidget {
             onPressed: () {},
           ),
           IconButton(
-            icon: const Icon(
-                IconlyLight.notification, color: FigmaColors.primary500),
+            icon: const Icon(IconlyLight.notification,
+                color: FigmaColors.primary500),
             onPressed: () {},
           )
-        ]
-    );
+        ]);
   }
 }
 
@@ -199,8 +195,8 @@ class _SuggestionsContentState extends State<SuggestionsContent> {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text(
-          'Recommendations', style: TextStyle(color: FigmaColors.greyscale900)),
+      child: Text('Recommendations',
+          style: TextStyle(color: FigmaColors.greyscale900)),
     );
   }
 }
@@ -243,12 +239,11 @@ class ExploreAppBar extends StatelessWidget with PreferredSizeWidget {
             onPressed: () {},
           ),
           IconButton(
-            icon: const Icon(
-                IconlyLight.notification, color: FigmaColors.primary500),
+            icon: const Icon(IconlyLight.notification,
+                color: FigmaColors.primary500),
             onPressed: () {},
           )
-        ]
-    );
+        ]);
   }
 }
 
@@ -260,10 +255,26 @@ class ExploreContent extends StatefulWidget {
 }
 
 class _ExploreContentState extends State<ExploreContent> {
+  late ScrollController _scrollController;
+  double _scrollControllerOffset = 0.0;
+
+  _scrollListener() {
+    setState(() {
+      _scrollControllerOffset = _scrollController.offset;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Explore', style: TextStyle(color: FigmaColors.greyscale900)),
+    return CustomScrollView(
+      controller: _scrollController,
     );
   }
 }
@@ -277,6 +288,50 @@ class ExploreScreen extends StatelessWidget {
       appBar: ExploreAppBar(),
       body: const ExploreContent(),
     );
+  }
+}
+
+class SearchInput extends StatelessWidget {
+  const SearchInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 50,
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              offset: const Offset(12, 26),
+              blurRadius: 50,
+              spreadRadius: 0,
+              color: Colors.grey.withOpacity(.1)),
+        ]),
+        child: TextField(
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 14),
+          decoration: InputDecoration(
+            // prefixIcon: Icon(Icons.email),
+            prefixIcon:
+                const Icon(Icons.search, size: 20, color: Color(0xffFF5A60)),
+            filled: true,
+            fillColor: Colors.white,
+            hintText: 'Where are you going?',
+            hintStyle: TextStyle(color: Colors.black.withOpacity(.75)),
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 1.0),
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 2.0),
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            ),
+          ),
+          onChanged: (value) {},
+        ));
   }
 }
 //#endregion
@@ -302,16 +357,16 @@ class SavedAppBar extends StatelessWidget with PreferredSizeWidget {
         ),
         actions: [
           IconButton(
-            icon: const Icon(IconlyLight.edit_square, color: FigmaColors.primary500),
+            icon: const Icon(IconlyLight.edit_square,
+                color: FigmaColors.primary500),
             onPressed: () {},
           ),
           IconButton(
-            icon: const Icon(
-                IconlyLight.notification, color: FigmaColors.primary500),
+            icon: const Icon(IconlyLight.notification,
+                color: FigmaColors.primary500),
             onPressed: () {},
           )
-        ]
-    );
+        ]);
   }
 }
 
@@ -323,6 +378,22 @@ class SavedContent extends StatefulWidget {
 }
 
 class _SavedContentState extends State<SavedContent> {
+  late ScrollController _scrollController;
+  double _scrollControllerOffset = 0.0;
+
+  _scrollListener() {
+    setState(() {
+      _scrollControllerOffset = _scrollController.offset;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Center(
@@ -365,16 +436,16 @@ class ProfileAppBar extends StatelessWidget with PreferredSizeWidget {
         ),
         actions: [
           IconButton(
-            icon: const Icon(IconlyLight.edit_square, color: FigmaColors.primary500),
+            icon: const Icon(IconlyLight.edit_square,
+                color: FigmaColors.primary500),
             onPressed: () {},
           ),
           IconButton(
-            icon: const Icon(
-                IconlyLight.notification, color: FigmaColors.primary500),
+            icon: const Icon(IconlyLight.notification,
+                color: FigmaColors.primary500),
             onPressed: () {},
           )
-        ]
-    );
+        ]);
   }
 }
 
@@ -386,6 +457,22 @@ class ProfileContent extends StatefulWidget {
 }
 
 class _ProfileContentState extends State<ProfileContent> {
+  late ScrollController _scrollController;
+  double _scrollControllerOffset = 0.0;
+
+  _scrollListener() {
+    setState(() {
+      _scrollControllerOffset = _scrollController.offset;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Center(
@@ -428,12 +515,11 @@ class MoreAppBar extends StatelessWidget with PreferredSizeWidget {
         ),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(
-                IconlyLight.notification, color: FigmaColors.primary500),
+            icon: const Icon(IconlyLight.notification,
+                color: FigmaColors.primary500),
             onPressed: () {},
           )
-        ]
-    );
+        ]);
   }
 }
 
@@ -445,6 +531,22 @@ class MoreContent extends StatefulWidget {
 }
 
 class _MoreContentState extends State<MoreContent> {
+  late ScrollController _scrollController;
+  double _scrollControllerOffset = 0.0;
+
+  _scrollListener() {
+    setState(() {
+      _scrollControllerOffset = _scrollController.offset;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Center(
