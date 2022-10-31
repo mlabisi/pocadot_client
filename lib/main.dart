510f6d3d6bd6111916ea4707c8c074ab3d6c8cde
Mora <mlabisi98@gmail.com>
2022-10-30 21:30:09 -0700

    adding menu bar ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pocadot_client/theme/colors.dart';
+import 'package:iconly/iconly.dart';
+import 'package:pocadot_client/theme/icons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,6 +51,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  IconData? _selectedIcon;
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -105,11 +110,100 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: const BottomNavBar(),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.white,
+      child: SizedBox(
+        height: 56,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconBottomBar(
+                  icon: PocadotIcons.cards,
+                  activeIcon: PocadotIcons.cardsFilled,
+                  selected: _selectedIndex == 0,
+                  onPressed: () => _onItemTapped(0)),
+              IconBottomBar(
+                  icon: IconlyLight.search,
+                  activeIcon: IconlyBold.search,
+                  selected: _selectedIndex == 1,
+                  onPressed: () => _onItemTapped(1)),
+              IconBottomBar(
+                  icon: IconlyLight.heart,
+                  activeIcon: IconlyBold.heart,
+                  selected: _selectedIndex == 2,
+                  onPressed: () => _onItemTapped(2)),
+              IconBottomBar(
+                  icon: IconlyLight.profile,
+                  activeIcon: IconlyBold.profile,
+                  selected: _selectedIndex == 3,
+                  onPressed: () => _onItemTapped(3)),
+              IconBottomBar(
+                  icon: IconlyLight.more_circle,
+                  activeIcon: IconlyBold.more_circle,
+                  selected: _selectedIndex == 4,
+                  onPressed: () => _onItemTapped(4))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class IconBottomBar extends StatelessWidget {
+  const IconBottomBar(
+      {Key? key,
+        required this.activeIcon,
+        required this.icon,
+        required this.selected,
+        required this.onPressed})
+      : super(key: key);
+  final IconData activeIcon;
+  final IconData icon;
+  final bool selected;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            selected ? activeIcon : icon,
+            size: 25,
+            color: selected ? FigmaColors.primary500 : Colors.black54,
+          ),
+        ),
+      ],
     );
   }
 }
