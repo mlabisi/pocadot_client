fb5d1e6eeddc32bec7b65c673f4f4b78fdc5635a
Mora <mlabisi98@gmail.com>
2022-10-30 17:27:54 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/pocadotapp/pocadot_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pocadotapp.pocadot_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/mlabisi98/IdeaProjects/pocadot_client/ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
