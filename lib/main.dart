7c450c92f23c337d990eec1eb6c1a55c9a971c76
Mora <mlabisi98@gmail.com>
2022-10-31 18:12:30 -0700

    starting to add graphql

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:pocadot_client/theme/colors.dart';
 import 'package:iconly/iconly.dart';
 import 'package:pocadot_client/theme/icons.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // We're using HiveStore for persistence,
+  // so we need to initialize Hive.
+  await initHiveForFlutter();
+
+  final HttpLink httpLink = HttpLink(
+    'http://localhost:9002/graphql',
+  );
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: httpLink,
+      // The default store is the InMemoryStore, which does NOT persist to disk
+      cache: GraphQLCache(store: HiveStore()),
+    ),
+  );
+
+  runApp(MyApp(client: client));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.client});
+
+  final ValueNotifier<GraphQLClient> client;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'pocadot',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: FigmaColors.primary500,
+    return GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+        title: 'pocadot',
+        theme: ThemeData(
+          primaryColor: FigmaColors.primary500,
+        ),
+        home: const Root(),
       ),
-      home: const Root(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class Root extends StatefulWidget {
-  const Root({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const Root({super.key});
 
   @override
   State<Root> createState() => _RootState();
@@ -69,12 +71,6 @@ class _RootState extends State<Root> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         body: _pages[_selectedIndex],
         bottomNavigationBar: BottomAppBar(
