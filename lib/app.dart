77fdf83b90ffad7321a0912cb89c7744cdfcf372
Mora <mlabisi98@gmail.com>
2022-11-01 18:04:31 -0700

    improving project structure some more

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:iconly/iconly.dart';
-import 'package:pocadot_client/explore/explore_screen.dart';
-import 'package:pocadot_client/more/more_screen.dart';
-import 'package:pocadot_client/profile/profile_screen.dart';
-import 'package:pocadot_client/saved/saved_screen.dart';
-import 'package:pocadot_client/suggestions/suggestions_screen.dart';
+import 'package:pocadot_client/domains/explore/explore_screen.dart';
+import 'package:pocadot_client/domains/more/more_screen.dart';
+import 'package:pocadot_client/domains/profile/my_profile_screen.dart';
+import 'package:pocadot_client/domains/saved/saved_screen.dart';
+import 'package:pocadot_client/domains/suggestions/suggestions_screen.dart';
 import 'package:pocadot_client/theme/colors.dart';
 import 'package:pocadot_client/theme/icons.dart';
 import 'package:provider/provider.dart';
