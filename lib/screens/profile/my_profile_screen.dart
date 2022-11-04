903de38b615ede16b9cea82c59ea9d861f18b801
Mora <mlabisi98@gmail.com>
2022-11-04 08:13:44 -0700

    fixing stub for my profile screen

--- lib/screens/profile/my_profile_screen.dart
+++ lib/screens/profile/my_profile_screen.dart
@@ -3,43 +3,9 @@ import 'package:pocadot_client/theme/colors.dart';
 import 'package:iconly/iconly.dart';
 import 'package:pocadot_client/theme/icons.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:pocadot_client/widgets/navigation/main_tab_app_bar.dart';
 
 //#region PROFILE
-class ProfileAppBar extends StatelessWidget with PreferredSizeWidget {
-  @override
-  final Size preferredSize;
-
-  ProfileAppBar({Key? key})
-      : preferredSize = const Size.fromHeight(56.0),
-        super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-        backgroundColor: PocadotColors.greyscale50,
-        elevation: 1,
-        centerTitle: false,
-        title: const Text(
-          "Profile",
-          style: TextStyle(color: PocadotColors.primary500, fontFamily: 'Jua'),
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(IconlyLight.edit_square,
-                color: PocadotColors.primary500),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(IconlyLight.notification,
-                color: PocadotColors.primary500),
-onPressed: () {
-            Navigator.pushNamed(context, '/notifications');
-          },
-          )
-        ]);
-  }
-}
-
 class ProfileContent extends StatefulWidget {
   const ProfileContent({super.key});
 
@@ -67,7 +33,8 @@ class _ProfileContentState extends State<ProfileContent> {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Profile', style: TextStyle(color: PocadotColors.greyscale900)),
+      child:
+          Text('Profile', style: TextStyle(color: PocadotColors.greyscale900)),
     );
   }
 }
@@ -78,7 +45,20 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ProfileAppBar(),
+      appBar: TabAppBar(title: 'My Profile', actions: [
+        IconButton(
+          icon: const Icon(IconlyLight.edit_square,
+              color: PocadotColors.primary500),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: const Icon(IconlyLight.notification,
+              color: PocadotColors.primary500),
+          onPressed: () {
+            Navigator.pushNamed(context, '/notifications');
+          },
+        )
+      ]),
       body: const ProfileContent(),
     );
   }
