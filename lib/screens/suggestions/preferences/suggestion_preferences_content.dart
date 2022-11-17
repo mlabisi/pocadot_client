ad3187e0b6868ac533fe9a0e6a9eb56be20a80d1
Mora <mlabisi98@gmail.com>
2022-11-16 20:13:11 -0800

    making preferences screen functional

--- lib/screens/suggestions/preferences/suggestion_preferences_content.dart
+++ lib/screens/suggestions/preferences/suggestion_preferences_content.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
 import 'package:pocadot_client/screens/suggestions/preferences/~graphql/__generated__/suggestion_preferences.fragment.graphql.dart';
 import 'package:pocadot_client/theme/colors.dart';
+import 'package:pocadot_client/theme/icons.dart';
 
 class SuggestionPreferencesContent extends StatefulWidget {
   final Fragment$suggestionPreferences suggestionPreferences;
   final Function refresh;
   final Function fetchMore;
 
-  const SuggestionPreferencesContent({Key? key, required this.suggestionPreferences, required this.refresh, required this.fetchMore}) : super(key: key);
+  const SuggestionPreferencesContent(
+      {Key? key,
+      required this.suggestionPreferences,
+      required this.refresh,
+      required this.fetchMore})
+      : super(key: key);
 
   @override
-  State<SuggestionPreferencesContent> createState() => _SuggestionPreferencesContentState();
+  State<SuggestionPreferencesContent> createState() =>
+      _SuggestionPreferencesContentState();
 }
 
-class _SuggestionPreferencesContentState extends State<SuggestionPreferencesContent> {
+class _SuggestionPreferencesContentState
+    extends State<SuggestionPreferencesContent> {
   late bool _biasesOnly;
 
   void toggleSwitch() {
@@ -32,221 +41,135 @@ class _SuggestionPreferencesContentState extends State<SuggestionPreferencesCont
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children:[
-        SizedBox(
-          width: 380,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children:[
-              const SizedBox(
-                width: double.infinity,
-                child: Text(
-                  "Personal Preferences",
-                  style: TextStyle(
-                    color: Color(0xff616161),
-                    fontSize: 14,
-                    fontFamily: "Urbanist",
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisSize: MainAxisSize.min,
+    return LayoutBuilder(builder: (context, constraints) {
+      return Padding(
+        padding: const EdgeInsets.only(left: (16), top: (18), right: (16)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Personal Preferences",
+                style: TextStyle(
+                  color: PocadotColors.greyscale700,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                )),
+            const SizedBox(height: 16),
+            const Divider(
+                thickness: 1,
+                indent: 0,
+                endIndent: 0,
+                color: PocadotColors.greyscale200),
+            const SizedBox(height: 16),
+            GestureDetector(
+              onTap: () => {},
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children:const [
-                  SizedBox(
-                    width: 380,
-                    height: 1,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children:[
-                    Container(
-                      width: 51.11,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        color: const Color(0x14a3b0ef),
-                      ),
-                      padding: const EdgeInsets.all(16),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children:[
-                          Container(
-                            width: 19.11,
-                            height: 24,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: const FlutterLogo(size: 19.114229202270508),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    Expanded(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:const [
-                          SizedBox(
-                            width: double.infinity,
-                            child: Text(
-                              "My Biases",
-                              style: TextStyle(
-                                color: Color(0xff212121),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      width: 48,
+                      height: 48,
+                      decoration: const BoxDecoration(
+                          color: PocadotColors.transparentBlue,
+                          shape: BoxShape.circle),
+                      child: Center(
+                        child: Text(
+                          String.fromCharCode(
+                              PocadotIcons.fingerHeart.codePoint),
+                          style: TextStyle(
+                              inherit: false,
+                              color: PocadotColors.primary500,
+                              fontSize: 32.0,
+                              fontWeight: FontWeight.w900,
+                              fontFamily: PocadotIcons.fingerHeart.fontFamily,
+                              package: PocadotIcons.fingerHeart.fontPackage),
+                        ),
+                      )),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            "My Biases",
+                            style: TextStyle(
+                                color: PocadotColors.greyscale900,
                                 fontSize: 18,
-                              ),
+                                fontFamily: 'Jua'),
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            "Update your biases to help pocadot make better recommendations!",
+                            style: TextStyle(
+                              color: PocadotColors.greyscale700,
+                              fontSize: 16,
+                              fontFamily: "Urbanist",
+                              fontWeight: FontWeight.w500,
                             ),
                           ),
-                          SizedBox(height: 4),
-                          SizedBox(
-                            width: double.infinity,
-                            child: Text(
-                              "Update your biases to help pocadot make better recommendations!",
-                              style: TextStyle(
-                                color: Color(0xff616161),
-                                fontSize: 16,
-                                fontFamily: "Urbanist",
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(width: 20),
-                    SizedBox(
-                      width: 20,
-                      height: 20,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children:[
-                          Container(
-                            width: 20,
-                            height: 20,
-                            padding: const EdgeInsets.only(left: 10786, top: 3, bottom: 11, ),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children:[
-                                Transform.rotate(
-                                  angle: -1.57,
-                                  child: Container(
-                                    width: 13.17,
-                                    height: 7.33,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(8),
-                                      border: Border.all(color: const Color(0xff212121), width: 1.50, ),
-                                      color: const Color(0x7f7f3a44),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(height: 35),
-        SizedBox(
-          width: 380,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children:[
-              const SizedBox(
-                width: double.infinity,
-                child: Text(
-                  "Recommendation Preferences",
-                  style: TextStyle(
-                    color: Color(0xff616161),
-                    fontSize: 14,
-                    fontFamily: "Urbanist",
-                    fontWeight: FontWeight.w700,
                   ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children:const [
-                  SizedBox(
-                    width: 380,
-                    height: 1,
+                  const Icon(
+                    IconlyLight.arrow_right_2,
+                    color: PocadotColors.greyscale900,
+                    size: 24.0,
                   ),
                 ],
               ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children:[
-                    SizedBox(
-                      width: double.infinity,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children:[
-                          const Expanded(
-                            child: SizedBox(
-                              child: Text(
-                                "Only show me listings that feature one of my biases",
-                                style: TextStyle(
-                                  color: Color(0xff212121),
-                                  fontSize: 16,
-                                  fontFamily: "Urbanist",
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 20),
-                          Switch(value: _biasesOnly, onChanged: (value) => toggleSwitch())
-                        ],
+            ),
+            const SizedBox(height: 30),
+            const Text("Recommendation Preferences",
+                style: TextStyle(
+                  color: PocadotColors.greyscale700,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                )),
+            const SizedBox(height: 16),
+            const Divider(
+                thickness: 1,
+                indent: 0,
+                endIndent: 0,
+                color: PocadotColors.greyscale200),
+            SizedBox(
+              width: double.infinity,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Expanded(
+                    child: Text(
+                      "Only show me listings that feature one of my biases",
+                      style: TextStyle(
+                        color: PocadotColors.greyscale900,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(width: 20),
+                  Switch(
+                      value: _biasesOnly,
+                      activeColor: PocadotColors.primary500,
+                      onChanged: (value) => toggleSwitch())
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
-    );
+      );
+    });
   }
 }
